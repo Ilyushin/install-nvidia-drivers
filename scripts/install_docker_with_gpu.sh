8bba88312bc863ed44e63164ccd8cff93f53e066
Eugene Ilyushin <john.ilyushin@gmail.com>
2020-04-18 10:37:15 +0300

    Fixed apt install

--- scripts/install_docker_with_gpu.sh
+++ scripts/install_docker_with_gpu.sh
@@ -5,7 +5,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -22,7 +22,7 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
 
